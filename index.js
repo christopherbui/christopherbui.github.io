de009201181882bf141293bf3bfcab3a32ce53ff
christopherbui <christopherbui@pm.me>
2024-09-22 01:29:36 -0700

    lots of updates

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // Ensure the page always starts at the top after a refresh
+    if ('scrollRestoration' in history) {
+        history.scrollRestoration = 'manual';
+    }
+
+    // Typing animation for the description
     const text = "I am a data scientist with a background in Bioinformatics, Machine Learning, and healthcare analytics. I have experience optimizing large scale data pipelines and developing predictive models. I am constantly open to learning and am driven to apply my analytical and interpersonal skillset to discover innovative solutions for highly-impactful challenges.";
-    const typingSpeed = 8; // Adjust the typing speed (milliseconds between each character)
+    const typingSpeed = 7; // Adjust the typing speed (milliseconds between each character)
     let i = 0;
     const descriptionElement = document.getElementById('description');
 
@@ -15,9 +21,10 @@ document.addEventListener("DOMContentLoaded", function () {
     // Start typing animation when the page loads
     typeWriter();
 
-    // Add event listener for the "Home" link to reload the page
+    // Add event listener for the "Home" link to reload the page and reset the URL
     document.querySelector('a[href="#home"]').addEventListener('click', function (e) {
         e.preventDefault(); // Prevent default anchor behavior
-        location.reload(); // Reload the page
+        history.replaceState(null, null, window.location.pathname); // Remove the #section from the URL
+        location.reload(); // Fully reload the page
     });
 });
